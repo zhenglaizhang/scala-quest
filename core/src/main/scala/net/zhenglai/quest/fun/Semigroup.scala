1a91bfe860acddfa170f27c7e7a03f7567f9acc9
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-07-13 18:21:09 +0800

    cats semigroup & monoid

--- core/src/main/scala/net/zhenglai/quest/fun/Semigroup.scala
+++ core/src/main/scala/net/zhenglai/quest/fun/Semigroup.scala
@@ -1,5 +1,6 @@
 package net.zhenglai.quest.fun
 
+// A semigroup is any set `A` with an associative operation (`combine`).
 // e.g. NonEmptyList, Positive integer addition
 trait Semigroup[@specialized(Int, Long, Float, Double) A] {
   // closed, associated, binary operation
--- core/src/test/scala/net/zhenglai/quest/CatsMonoidTest.scala
+++ core/src/test/scala/net/zhenglai/quest/CatsMonoidTest.scala
@@ -26,9 +26,28 @@ class CatsMonoidTest extends FunSuite {
     assert(Monoid.empty[String] == "")
   }
 
+  test("Cats.Monoid option[string]") {
+    import cats.Monoid
+    import cats.instances.option._
+    import cats.instances.string._
+    assert(Monoid[Option[String]].combine(Some("1"), None).contains("1"))
+    assert(Monoid[Option[String]].combine(Some("1"), Some("2")).contains("12"))
+  }
+
+  test("Cats.Monoid[List]") {
+    import cats.Monoid
+    import cats.instances.list._
+    assert(Monoid[List[String]].combine(List("a", "b"), List("c")) == List("a", "b", "c"))
+  }
+
   test("Cats.Semigroup") {
+    import cats.Monoid
     import cats.Semigroup
     import cats.instances.string._
     assert(Semigroup[String].combine("a", "b") == "ab")
+
+    import cats.syntax.semigroup._
+    assert(("a" |+| "b" |+| Monoid[String].empty) == "ab")
   }
+
 }
