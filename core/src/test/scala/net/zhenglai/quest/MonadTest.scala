acdbcbe6173f30f1060350778da25889b5af1997
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-07-14 13:50:40 +0800

    use for comprehension for monad

--- core/src/test/scala/net/zhenglai/quest/MonadTest.scala
+++ core/src/test/scala/net/zhenglai/quest/MonadTest.scala
@@ -6,17 +6,18 @@ import org.scalatest.{FunSuite, Matchers}
 
 class MonadTest extends FunSuite with Matchers {
 
+  // monads are truly about sequencing
   test("Option monad -> fail fast error handling") {
     def parseInt(s: String): Option[Int] = Try(s.toInt).toOption
 
     def divide(a: Int, b: Int): Option[Int] = if (b == 0) None else Some(a / b)
 
     def stringDivide(aStr: String, bStr: String): Option[Int] =
-      parseInt(aStr).flatMap { aNum =>
-        parseInt(bStr).flatMap { bNum =>
-          divide(aNum, bNum)
-        }
-      }
+      for {
+        aNum <- parseInt(aStr)
+        bNum <- parseInt(bStr)
+        res <- divide(aNum, bNum)
+      } yield res
 
     stringDivide("1", "3") should be(Some(0))
     stringDivide("str", "1") should be(None)
